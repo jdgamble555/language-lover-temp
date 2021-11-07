ffc1149f0b1c5632d658d9d21e3276ee4cd714c4
Jonathan Gamble <jdgamble555@gmail.com>
2021-11-06 20:25:31 -0500

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,39 @@
+<svelte:head>
+	<meta name="description" content="A website with links on language learning, and language resources!">
+</svelte:head>
+<script type="ts">
+	import { Navbar, Card } from 'spaper';
+	import 'papercss/dist/paper.min.css';
+</script>
+
+<nav>
+	<Navbar>
+		<h3 slot="brand">
+			<a href="/"><span class="color-red">❤</span> Language Lover</a>
+		</h3>
+		<ul class="inline">
+			<li><a href="/">Login</a></li>
+		</ul>
+	</Navbar>
+</nav>
+<div class="s-container">
+	<Card>
+		<slot />
+	</Card>
+</div>
+
+<style>
+	.s-container {
+		margin-top: 15px;
+		margin-bottom: 15px;
+		max-width: 995px;
+        width: 100%;
+		background-color: transparent !important;
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+    .color-red {
+        color: rgb(150, 2, 2);
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+	import { Alert } from 'spaper';
+</script>
+
+<svelte:head>
+	<title>Language Lover</title>
+</svelte:head>
+
+{#if false}<slot />{/if}
+
+<h1>Welcome to Language Lover</h1>
+
+<Alert>This website is coming soon! Your guide to language learning!</Alert>
